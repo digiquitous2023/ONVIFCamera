4aeb3e6ae328d8ec7b8d9a78efb04189bb8060b4
digiquitous2023 <chad@digiquitous.com>
2024-02-21 11:19:14 +0900

    change

--- Example/ONVIFCamera/forVLCLink.mm
+++
@@ -1,9 +0,0 @@
-//
-//  forVLCCompilation.m
-//  ONVIFCamera_Example
-//
-//  Created by Rémy Virin on 17/01/2018.
-//  Copyright © 2018 CocoaPods. All rights reserved.
-//
-
-// Needed to prevent link error from VLC 
